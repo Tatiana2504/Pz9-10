bd48b331a98bf200aebc3fb0e5e557fb2900b593
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-03-02 22:23:03 +0300

    1/3

--- Main.java
+++ Main.java
@@ -20,14 +20,12 @@ class JThread extends Thread {
 public class Main {
 
     public static void main(String[] args) {
-
         System.out.println("Main thread started...");
-        new JThread("JThread").start();
+        for (int i = 1; i < 6; i++)
+            new JThread("JThread " + i).start();
         System.out.println("Main thread finished...");
     }
 }
-
-
 
 
 
