b35a49c93bc7a449620cd5bf9529d9ddf49b0834
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-03-02 22:32:57 +0300

    1/8

--- Main.java
+++ Main.java
@@ -1,40 +1,40 @@
 package com.metanit;
-class JThread extends Thread {
-
-    JThread(String name){
-        super(name);
-    }
-    public void run(){
-
-        System.out.printf("%s started... \n", Thread.currentThread().getName());
-        int counter=1; // счетчик циклов
-        while(!isInterrupted()){
-
-            System.out.println("Loop " + counter++);
-        }
-        System.out.printf("%s finished... \n", Thread.currentThread().getName());
-    }
-}
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Main thread started...");
-        JThread t = new JThread("JThread");
-        t.start();
-        try{
-            Thread.sleep(150);
-            t.interrupt();
+        CommonResource commonResource= new CommonResource();
+        for (int i = 1; i < 6; i++){
 
-            Thread.sleep(150);
+            Thread t = new Thread(new CountThread(commonResource));
+            t.setName("Thread "+ i);
+            t.start();
         }
-        catch(InterruptedException e){
-            System.out.println("Thread has been interrupted");
-        }
-        System.out.println("Main thread finished...");
     }
 }
+class CommonResource{
 
+    int x=0;
+}
+
+class CountThread implements Runnable{
+
+    CommonResource res;
+    CountThread(CommonResource res){
+        this.res=res;
+    }
+    public void run(){
+        res.x=1;
+        for (int i = 1; i < 5; i++){
+            System.out.printf("%s %d \n", Thread.currentThread().getName(), res.x);
+            res.x++;
+            try{
+                Thread.sleep(100);
+            }
+            catch(InterruptedException e){}
+        }
+    }
+}
 
 
 
