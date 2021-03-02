ae7c5b65d9679bfe8d4e4af1c90fbd33bf27dce9
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-03-02 22:36:26 +0300

    1/10

--- Main.java
+++ Main.java
@@ -3,37 +3,66 @@ package com.metanit;
 public class Main {
 
     public static void main(String[] args) {
-        CommonResource commonResource= new CommonResource();
-        for (int i = 1; i < 6; i++){
+        Store store=new Store();
+        Producer producer = new Producer(store);
+        Consumer consumer = new Consumer(store);
+        new Thread(producer).start();
+        new Thread(consumer).start();
+    }
+}
+// Класс Магазин, хранящий произведенные товары
+class Store{
+    private int product=0;
+    public synchronized void get() {
+        while (product<1) {
+            try {
+                wait();
+            }
+            catch (InterruptedException e) {
+            }
+        }
+        product--;
+        System.out.println("Покупатель купил 1 товар");
+        System.out.println("Товаров на складе: " + product);
+        notify();
+    }
+    public synchronized void put() {
+        while (product>=3) {
+            try {
+                wait();
+            }
+            catch (InterruptedException e) {
+            }
+        }
+        product++;
+        System.out.println("Производитель добавил 1 товар");
+        System.out.println("Товаров на складе: " + product);
+        notify();
+    }
+}
+// класс Производитель
+class Producer implements Runnable{
 
-            Thread t = new Thread(new CountThread(commonResource));
-            t.setName("Thread "+ i);
-            t.start();
+    Store store;
+    Producer(Store store){
+        this.store=store;
+    }
+    public void run(){
+        for (int i = 1; i < 6; i++) {
+            store.put();
         }
     }
 }
-class CommonResource{
+// Класс Потребитель
+class Consumer implements Runnable{
 
-    int x=0;
-}
-
-class CountThread implements Runnable{
-
-    CommonResource res;
-    CountThread(CommonResource res){
-        this.res=res;
+    Store store;
+    Consumer(Store store){
+        this.store=store;
     }
     public void run(){
-        synchronized(res){
-            res.x=1;
-            for (int i = 1; i < 5; i++){
-                System.out.printf("%s %d \n", Thread.currentThread().getName(), res.x);
-                res.x++;
-                try{
-                    Thread.sleep(100);
-                }
-                catch(InterruptedException e){}
-            }
+        for (int i = 1; i < 6; i++) {
+            store.get();
         }
     }
 }
