521f1b533da8859c8e53bd41a1081e404f31c8c3
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-03-02 22:34:51 +0300

    1/9

--- Main.java
+++ Main.java
@@ -24,17 +24,18 @@ class CountThread implements Runnable{
         this.res=res;
     }
     public void run(){
-        res.x=1;
-        for (int i = 1; i < 5; i++){
-            System.out.printf("%s %d \n", Thread.currentThread().getName(), res.x);
-            res.x++;
-            try{
-                Thread.sleep(100);
+        synchronized(res){
+            res.x=1;
+            for (int i = 1; i < 5; i++){
+                System.out.printf("%s %d \n", Thread.currentThread().getName(), res.x);
+                res.x++;
+                try{
+                    Thread.sleep(100);
+                }
+                catch(InterruptedException e){}
             }
-            catch(InterruptedException e){}
         }
     }
 }
-
 
 
