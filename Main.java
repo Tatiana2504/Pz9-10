bddba68dcac18fd1601a791c7d2e78fe8c9d559f
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-03-02 22:24:57 +0300

    1/4

--- Main.java
+++ Main.java
@@ -21,8 +21,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Main thread started...");
-        for (int i = 1; i < 6; i++)
-            new JThread("JThread " + i).start();
+        JThread t= new JThread("JThread ");
+        t.start();
+        try{
+            t.join();
+        }
+        catch(InterruptedException e){
+
+            System.out.printf("%s has been interrupted", t.getName());
+        }
         System.out.println("Main thread finished...");
     }
 }
