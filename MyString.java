edaeee0b66123dc747b55bde58b7e0f25ac76bb0
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-03-02 23:05:14 +0300

    2/2(b)

---
+++ MyString.java
@@ -0,0 +1,23 @@
+package com.metanit;
+import java.awt.*;
+public class MyString extends Object{
+    int x,y,deltax,deltay;
+    String str;
+    public MyString(String str, int x, int y, int deltax, int deltay) {
+        this.x = x;
+        this.y = y;
+        this.deltax = deltax;
+        this.deltay = deltay;
+        this.str = str;
+    }
+
+    public void draw(Graphics g) {
+        g.drawString(str,x,y);
+    }
+
+    public void move(int width, int height) {
+        x = x+ deltax;
+        y = y+deltay;
+    }
+}
+
