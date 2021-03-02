81ace3c60e0669a665762dcaa0b672f41b4d7f17
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-03-02 22:28:52 +0300

    1/6

--- Main.java
+++ Main.java
@@ -19,7 +19,17 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Main thread started...");
-        Thread myThread = new Thread(new MyThread(),"MyThread");
+        Runnable r = ()->{
+            System.out.printf("%s started... \n", Thread.currentThread().getName());
+            try{
+                Thread.sleep(500);
+            }
+            catch(InterruptedException e){
+                System.out.println("Thread has been interrupted");
+            }
+            System.out.printf("%s finished... \n", Thread.currentThread().getName());
+        };
+        Thread myThread = new Thread(r,"MyThread");
         myThread.start();
         System.out.println("Main thread finished...");
     }
