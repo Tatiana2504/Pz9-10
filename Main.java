255d3b853c6fc7a4128399d955928d41f7a1e608
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-03-02 22:38:12 +0300

    1/11

--- Main.java
+++ Main.java
@@ -1,70 +1,48 @@
 package com.metanit;
-
+import java.util.concurrent.Semaphore;
 public class Main {
 
     public static void main(String[] args) {
-        Store store=new Store();
-        Producer producer = new Producer(store);
-        Consumer consumer = new Consumer(store);
-        new Thread(producer).start();
-        new Thread(consumer).start();
+        Semaphore sem = new Semaphore(1); // 1 разрешение
+        CommonResource res = new CommonResource();
+        new Thread(new CountThread(res, sem, "CountThread 1")).start();
+        new Thread(new CountThread(res, sem, "CountThread 2")).start();
+        new Thread(new CountThread(res, sem, "CountThread 3")).start();
     }
 }
-// Класс Магазин, хранящий произведенные товары
-class Store{
-    private int product=0;
-    public synchronized void get() {
-        while (product<1) {
-            try {
-                wait();
-            }
-            catch (InterruptedException e) {
-            }
-        }
-        product--;
-        System.out.println("Покупатель купил 1 товар");
-        System.out.println("Товаров на складе: " + product);
-        notify();
-    }
-    public synchronized void put() {
-        while (product>=3) {
-            try {
-                wait();
-            }
-            catch (InterruptedException e) {
-            }
-        }
-        product++;
-        System.out.println("Производитель добавил 1 товар");
-        System.out.println("Товаров на складе: " + product);
-        notify();
-    }
-}
-// класс Производитель
-class Producer implements Runnable{
+class CommonResource{
 
-    Store store;
-    Producer(Store store){
-        this.store=store;
-    }
-    public void run(){
-        for (int i = 1; i < 6; i++) {
-            store.put();
-        }
-    }
+    int x=0;
 }
-// Класс Потребитель
-class Consumer implements Runnable{
 
-    Store store;
-    Consumer(Store store){
-        this.store=store;
+class CountThread implements Runnable{
+
+    CommonResource res;
+    Semaphore sem;
+    String name;
+    CountThread(CommonResource res, Semaphore sem, String name){
+        this.res=res;
+        this.sem=sem;
+        this.name=name;
     }
+
     public void run(){
-        for (int i = 1; i < 6; i++) {
-            store.get();
+
+        try{
+            System.out.println(name + " ожидает разрешение");
+            sem.acquire();
+            res.x=1;
+            for (int i = 1; i < 5; i++){
+                System.out.println(this.name + ": " + res.x);
+                res.x++;
+                Thread.sleep(100);
+            }
         }
+        catch(InterruptedException e){System.out.println(e.getMessage());}
+        System.out.println(name + " освобождает разрешение");
+        sem.release();
     }
-}
+    }
+
 
 
